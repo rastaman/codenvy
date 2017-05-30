aeda612d113d529f0235aff5a460b17db0b39d63
Michail Kuznetsov <mkuznyetsov@codenvy.com>
2017-05-30 07:20:29 +0000

    Fix build accordingly to changes from CHE-5060 (#2241)

--- wsmaster/codenvy-hosted-integration-tests/codenvy-integration-cascade-jpa/src/test/java/com/codenvy/integration/jpa/cascaderemoval/TestObjectsFactory.java
+++ wsmaster/codenvy-hosted-integration-tests/codenvy-integration-cascade-jpa/src/test/java/com/codenvy/integration/jpa/cascaderemoval/TestObjectsFactory.java
@@ -97,7 +97,6 @@ public final class TestObjectsFactory {
                                new AuthorImpl(creator, System.currentTimeMillis()),
                                null,
                                null,
-                               null,
                                null);
     }
 
